8020713833e20fce2bb0d16d941a14f2b6276ddb
CHAKFI <ahmedchakfi1999@gmail.com>
2018-07-09 16:35:34 +0100

    upd_calc

--- Calculator.java
+++ Calculator.java
@@ -2,18 +2,19 @@ import java.util.Scanner;
 public class Calculator {
 public static void main(String[] args) {
          int a,b,s;
-         char chx;
-     System.out.println("BONJOURS (*o*) \n Veuillez saisir l'opération (deux nombre pas plus!)");    
+         char chx,r = '=';
+     System.out.println("BONJOURS (*o*) \n Veuillez saisir l'opération (deux nombre pas plus!)\n(+,-,*,^,/)");    
      Scanner sc = new Scanner(System.in);
      a = sc.nextInt();chx = sc.next().charAt(0);b= sc.nextInt();
      switch(chx) {
-     case '+' : s=a+b; System.out.println("La Solution est : "+s);
+     case '+' : s=a+b; System.out.println("-----> "+a+chx+b+r+s);
      break; 
-     case '-' : s=a-b; System.out.println("La Solution est : "+s);
+     case '-' : s=a-b; System.out.println("-----> "+a+chx+b+r+s);
      break; 
-     case '*' : s=a*b; System.out.println("La Solution est : "+s);
+     case '*' : s=a*b; System.out.println("-----> "+a+chx+b+r+s);
      break;
-     case '/' : if(b==0) System.out.println("Impossible de diviser par : 0 (");    
-                else System.out.println("La Solution est : "+a/b);
+     case '/' : s=a/b; if(b==0) System.out.println("Impossible de diviser par : 0 (");    
+                else System.out.println("-----> "+a+chx+b+r+s);
      break;
+     case '^' : s=a^b; System.out.println("");
      default : System.out.println("Choix introuvable (-_-) "); } } }
