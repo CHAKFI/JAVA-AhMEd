512fe74d04e4b2a1c489e0f22955abed161cd048
CHAKFI <ahmedchakfi1999@gmail.com>
2018-07-10 17:55:47 +0100

    "Bonjours" (while)

---
+++ bjr_while.java
@@ -0,0 +1,11 @@
+public class bjr_while {
+  public static void main(String[] args) {
+	  
+	  int i=0;
+	 
+	 while(i<5) {
+		 System.out.println("~~ Bonjours les programmeurs ~~");
+		 i=i+1;
+	   }
+     } 
+   }
